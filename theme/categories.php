b9993b9e2e05c5f84ee708512a84307fe6280cc5
rahularyan <rah12@live.com>
2016-03-27 17:15:24 +0530

    Fixed translation issue in categories page

--- theme/categories.php
+++ theme/categories.php
@@ -16,47 +16,49 @@
 <?php dynamic_sidebar( 'ap-top' ); ?>
 <div class="row">
 	<div id="ap-lists" class="<?php echo is_active_sidebar( 'ap-category' ) && is_anspress() ? 'col-md-9' : 'col-md-12' ?>">
-		<div id="ap-categories" class="clearfix">
-			<ul class="ap-term-category-box clearfix">
-				<?php foreach($question_categories as $key => $category) : ?>
-					<li class="clearfix">
-						<div class="ap-category-item">
-							<div class="ap-cat-img-c">
-								<span class="ap-term-count">
-									<?php printf(_n('%d Question', '%d Questions', 'categories-for-anspress', $category->count), $category->count) ?>
-								</span>
+        <div id="ap-categories" class="clearfix">
+            <ul class="ap-term-category-box clearfix">
+				<?php foreach ( $question_categories as $key => $category ) : ?>
+                    <li class="clearfix">
+                        <div class="ap-category-item">
+                            <div class="ap-cat-img-c">
+                                <span class="ap-term-count">
+									<?php printf(_n('%d Question', '%d Questions', $category->count, 'categories-for-anspress' ), $category->count ) ?>
+                                </span>
 								<a href="<?php echo get_category_link( $category );?>">
-									<?php echo ap_get_category_image($category->term_id); ?>
-								</a>
-							</div>
+									<?php echo ap_get_category_image($category->term_id ); ?>
+                                </a>
+                            </div>
 
-							<div class="ap-term-title">
+                            <div class="ap-term-title">
 								<a class="term-title" href="<?php echo get_category_link( $category );?>">
 									<?php echo $category->name; ?>
-								</a>
-								<?php $sub_cat_count = count(get_term_children( $category->term_id, 'question_category' )); ?>
-								<?php if($sub_cat_count > 0): ?>
-									<span class="ap-sub-category">
-									<?php printf(_n('%d Sub category', '%d Sub categories', 'categories-for-anspress', $sub_cat_count), $sub_cat_count) ?>
-									</span>
+                                </a>
+								<?php $sub_cat_count = count(get_term_children( $category->term_id, 'question_category' ) ); ?>
+								<?php if ( $sub_cat_count > 0 ) : ?>
+                                    <span class="ap-sub-category">
+										<?php 
+											printf(_n('%d Sub category', '%d Sub categories', $sub_cat_count, 'categories-for-anspress' ), $sub_cat_count );
+										?>
+                                    </span>
 								<?php endif; ?>
-							</div>
+                            </div>
 
-							<?php if($category->description != ''): ?>
-								<div class="ap-taxo-description">
-									<?php echo ap_truncate_chars($category->description, 70); ?>
-								</div>
+							<?php if ( $category->description != '' ) : ?>
+                                <div class="ap-taxo-description">
+									<?php echo ap_truncate_chars($category->description, 70 ); ?>
+                                </div>
 							<?php endif; ?>
-						</div>
-					</li>
+                        </div>
+                    </li>
 				<?php endforeach; ?>
-			</ul>
-		</div>
+            </ul>
+        </div>
 		<?php ap_pagination(); ?>
-	</div>
-	<?php if ( is_active_sidebar( 'ap-category' ) && is_anspress()){ ?>
-	<div class="ap-question-right col-md-3">
+    </div>
+	<?php if ( is_active_sidebar( 'ap-category' ) && is_anspress() ) { ?>
+    <div class="ap-question-right col-md-3">
 		<?php dynamic_sidebar( 'ap-category' ); ?>
-	</div>
+    </div>
 	<?php } ?>
 </div>
